ecad6001007f4317939b47dfe47f778683ccd02a
pingmac <zouguoping@lvmama.com>
2018-04-19 16:29:14 +0800

    ccc

--- main.go
+++ main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("master---new")
+	fmt.Println("master---new")
+	fmt.Println("master---new")
+	fmt.Println("master---new")
+	fmt.Println("master---new")
+	fmt.Println("master---new")
+	fmt.Println("master---new")
+	fmt.Println("master---new")
+
+
 }
