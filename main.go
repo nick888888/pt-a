f3e6cca07cb6ec83d142d895c82c7e77780652e8
pingmac <zouguoping@lvmama.com>
2018-05-08 13:43:18 +0800

    m 180508

--- main.go
+++ main.go
@@ -3,14 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("master---new")
-	fmt.Println("master---new")
-	fmt.Println("master---new")
-	fmt.Println("master---new")
-	fmt.Println("master---new")
-	fmt.Println("master---new")
-	fmt.Println("master---new")
-	fmt.Println("master---new")
-
-
+	fmt.Println("master180508")
 }
